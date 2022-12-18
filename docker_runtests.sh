5c6329ac7cdb34f096a048499a7ef33f9d869300
Julian Loiacono <jcloiacon@gmail.com>
2022-12-17 20:40:11 -0500

    CI testing

---
+++ docker_runtests.sh
@@ -0,0 +1,3 @@
+sudo apt install pip
+pip install -r requirements.txt
+python vulkanese/math/loiacono/loiacono_gpu.py
--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,1 @@
-sudo apt install pip
-pip install -r requirements.txt
 python vulkanese/math/loiacono/loiacono_gpu.py
