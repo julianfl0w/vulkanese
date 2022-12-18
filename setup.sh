6a46325e56db5f02c09d1bb957f5a298a69fcc57
Julian Loiacono <jcloiacon@gmail.com>
2022-12-18 18:31:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ sudo apt-get update -y
 sudo apt install python3-pip 
 sudo apt install python-is-python3
 sudo apt-get install -y mesa-vulkan-drivers
+sudo apt-get install ffmpeg libsm6 libxext6  -y
 pip install -r requirements.txt
 python vulkanese/math/loiacono/loiacono_gpu.py
