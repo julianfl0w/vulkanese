9b111d4ef13e4e3c94ac5d9d317217b4722c78ca
Julian Loiacono <jcloiacon@gmail.com>
2022-12-19 13:27:00 -0500

    Update su_setup.sh

--- su_setup.sh
+++ su_setup.sh
@@ -1,6 +1,8 @@
+add-apt-repository ppa:deadsnakes/ppa
 apt-get update -y
-apt install python3-pip 
-apt install python-is-python3
+apt-get install python3.9
+apt-get install python3-pip 
+apt-get install python-is-python3
 apt-get install -y mesa-vulkan-drivers
 apt-get install ffmpeg libsm6 libxext6  -y
 pip install -r -q requirements.txt
