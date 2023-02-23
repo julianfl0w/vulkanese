ed1c8fd34cd5b6408ce1f82b66e2f867290992f8
Julian Loiacono <jcloiacon@gmail.com>
2023-02-23 16:31:47 -0500

    get git

--- deployment/su_setup.sh
+++ deployment/su_setup.sh
@@ -5,4 +5,5 @@ apt-get install python3-pip  -y
 apt-get install python-is-python3 -y
 apt-get install -y mesa-vulkan-drivers -y
 apt-get install ffmpeg libsm6 libxext6  -y
+apt-get install git  -y
 pip install -r requirements.txt
